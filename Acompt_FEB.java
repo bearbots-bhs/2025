9ec1835676aed37a5e04b8a7009907c6ced531d8
luke89joseph <luke89joseph@gmail.com>
2025-12-11 15:39:03 -0500

    12/11/25
    Started working on code. Will have basic auto functions done next week; working on drivetrain Teleop

---
+++ Acompt_FEB.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.feb2026_lynn;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name = "ACompt February 2026")
+
+public class Acompt_FEB {
+    
+    private DcMotor backleftmotor;
+    private DcMotor frontrightmotor;
+    private DcMotor frontleftmotor;
+    private DcMotor backrightmotor;
+
+    // todo: write your code here
+    
+    public void runOpMode() {
+        backrightmotor = hardwareMap.get(DcMotor.class, "back right motor");
+        backleftmotor = hardwareMap.get(DcMotor.class, "back left motor");
+        frontrightmotor = hardwareMap.get(DcMotor.class, "front right motor");
+        frontleftmotor = hardwareMap.get(DcMotor.class, "front left motor");
+        
+        waitForStart();
+        
+        while (opModeIsActive()) {
+            x = gamepad1.right_stick_x;
+        }
+    }
+    
+}
---
+++ AutoMain_FEB.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.feb2026_lynn;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Auto February 2026")
+
+
+public class AutoMain_FEB {
+    private DcMotor backleftmotor;
+    private DcMotor frontrightmotor;
+    private DcMotor frontleftmotor;
+    private DcMotor backrightmotor;
+    
+    
+
+    // todo: write your code here
+    public void runOpMode() {
+        backrightmotor = hardwareMap.get(DcMotor.class, "back right motor");
+        backleftmotor = hardwareMap.get(DcMotor.class, "back left motor");
+        frontrightmotor = hardwareMap.get(DcMotor.class, "front right motor");
+        frontleftmotor = hardwareMap.get(DcMotor.class, "front left motor");
+        // linear code goes here
+    }
+    
+    public void stopMotors() {
+        backleftmotor.setPower(0);
+        backrightmotor.setPower(0);
+        frontleftmotor.setPower(0);
+        frontrightmotor.setPower(0);
+    }
+    
+    public void moveForward() {
+        backleftmotor.setPower(1);
+        backrightmotor.setPower(1);
+        frontleftmotor.setPower(1);
+        frontrightmotor.setPower(1);
+    }
+    
+    public void moveBackward() {
+        backleftmotor.setPower(-1);
+        frontleftmotor.setPower(1);
+        back
+    }
+    
+    public void strafeLeft() {
+        
+    }
+    
+    public void strafeRight() {
+        
+    }
+    
+    
+}
